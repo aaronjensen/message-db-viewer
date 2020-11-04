250cb64f5cafb94c826a827c6f14338d7eea3ee8
Aaron Jensen <aaronjensen@gmail.com>
2020-11-04 07:43:37 -0600

    Twin macro

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,11 @@
-import '../styles/tailwind.css';
+import { GlobalStyles } from 'twin.macro'
+import * as React from 'react'
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <GlobalStyles />
+      <Component {...pageProps} />
+    </>
+  )
 }
---
+++ pages/babel-plugin-macros.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  twin: {
+    config: 'tailwind.config.js',
+    preset: 'emotion',
+    debugProp: true,
+    debugPlugins: false,
+    debug: false,
+  },
+}
