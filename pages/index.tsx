f446458cb7c356b51976b19f2e863e53f8602ed3
Aaron Jensen <aaronjensen@gmail.com>
2020-11-04 07:43:41 -0600

    Basic styling

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,7 @@
 import Head from 'next/head'
 import { useRouter } from 'next/router'
 import useSWR from 'swr'
+import tw, { css, styled, theme } from 'twin.macro'
 
 const fetchJSON = url => fetch(url).then(res => res.json());
 
@@ -8,18 +9,25 @@ const Stream = ({ streamName }) => {
   const { data } = useSWR(`/api/stream/${streamName}`, fetchJSON)
 
   return (
-    <div>
-      {data.map(message => <Message message={message}/>)}
+    <div tw="grid grid-flow-col auto-cols-max gap-2 border p-2 bg-gray-100">
+      {data && data.map(message => (
+        <Message key={message.id} message={message}/>
+      ))}
     </div>
   )
 }
 
-const Message = ({message}) => {
+const Message = ({ message }) => {
   return (
-    <div>
+    <div tw="border px-4 py-2 bg-white flex flex-col">
       <div>{message.type}</div>
-      <div>Position: {message.position}</div>
-      <div>GP: {message.global_position}</div>
+      <div tw="grid grid-cols-2 gap-x-2">
+        <div>Pos:</div>
+        <div tw="text-right">{message.position}</div>
+
+        <div>GP:</div>
+        <div tw="text-right">{message.global_position}</div>
+      </div>
     </div>
   )
 
@@ -31,9 +39,9 @@ export default function Home() {
   const streamName = router.query.streamName
   console.log(streamName)
 
-
   return (
-    <div>
+    <div tw="p-4">
+      <div tw="mb-2">{streamName}</div>
       {streamName && <Stream streamName={streamName}/>}
     </div>
   )
