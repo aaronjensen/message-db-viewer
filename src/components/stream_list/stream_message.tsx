a9edaa5ec780b90d8beaf8599836606119eabd75
Aaron Jensen <aaronjensen@gmail.com>
2021-01-31 14:10:19 -0600

    Background is applied to body

--- src/components/stream_list/stream_message.tsx
+++ src/components/stream_list/stream_message.tsx
@@ -1,6 +1,5 @@
 import * as React from "react"
 import Xarrow from "@components/xarrow"
-import { usePopper } from "react-popper"
 import "twin.macro"
 import * as Data from "@data"
 
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,17 @@
-import { GlobalStyles } from "twin.macro"
+import tw, { GlobalStyles } from "twin.macro"
 import * as React from "react"
 import { AppProps } from "next/dist/next-server/lib/router/router"
+import { Global } from "@emotion/react"
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
       <GlobalStyles />
+      <Global
+        styles={{
+          body: tw`bg-gray-50`,
+        }}
+      />{" "}
       <Component {...pageProps} />
     </>
   )
